2290001ec25e7a25bfe60c468154869d1a9db3a6
AnthonyMTH <aticonaha@unsa.edu.pe>
2024-09-19 18:57:42 -0500

    Intent para registrar cuenta

---
+++ app/src/main/java/com/example/loginsample/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.loginsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.loginsample.databinding.ActivityMainBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        EditText edtUsername = binding.edtUsername;
+        EditText edtPassword = binding.edtPassword;
+        Button btnLogin = binding.btnLogin;
+        Button btnAddAccount = binding.btnAddAccount;
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtUsername.getText().toString().equals("admin") && edtPassword.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Bienvenido a mi App", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Bienvenido a mi App");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error en la autenticación", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Error en la autenticación");
+                }
+            }
+        });
+
+        btnAddAccount.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
+            startActivity(intent);
+        });
+    }
+}
